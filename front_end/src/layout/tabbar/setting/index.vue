9f55e7f0f183dd3678b2e14e3eb22fb817ea2a7d
Q <2904343971@qq.com>
2025-04-04 19:05:00 +0800

    全屏模式的切换功能

--- front_end/src/layout/tabbar/setting/index.vue
+++ front_end/src/layout/tabbar/setting/index.vue
@@ -1,6 +1,6 @@
 <template>
     <el-button type="primary" size="small" icon="Refresh" :circle="true" @click="updateRefresh"></el-button>
-    <el-button type="primary" size="small" icon="FullScreen" :circle="true"></el-button>
+    <el-button type="primary" size="small" icon="FullScreen" :circle="true" @click="fullScreen"></el-button>
     <el-button type="primary" size="small" icon="Setting" :circle="true"></el-button>
 
     <img src="@/assets/icons/AI.svg" style="width: 30px; height: 30px; margin:0px 10px ;" alt="userHead">
@@ -32,6 +32,18 @@ const layoutStore = useLayoutStore();
 
 const updateRefresh = () => {
     layoutStore.refresh = !layoutStore.refresh;
+}
+const fullScreen = () => {
+    //Dom对象的一个属性，判断当前是不是全屏模式
+    let full = document.fullscreenElement;
+    //切换全屏
+    if(!full){
+        //利用document对象的方法，让元素进入全屏模式
+        document.documentElement.requestFullscreen();
+    }else{
+        //退出全屏
+        document.exitFullscreen();
+    }
 }
 </script>
 
